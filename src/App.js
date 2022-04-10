2e945b19fb5e9b5dc754406127815f40dd698c08
christiannwamba <nwambachristian@gmail.com>
2022-04-10 13:15:10 +0400

    Password validation

--- src/App.js
+++ src/App.js
@@ -91,10 +91,19 @@ function App() {
       // https://stackoverflow.com/a/19605207
       password: Yup.string()
         .required('A password is required')
-        .matches(/(?=.*?[A-Z])/, 'Password must contain at least one uppercase letter')
-        .matches(/(?=.*?[a-z])/, 'Password must contain at least one lowercase letter')
+        .matches(
+          /(?=.*?[A-Z])/,
+          'Password must contain at least one uppercase letter'
+        )
+        .matches(
+          /(?=.*?[a-z])/,
+          'Password must contain at least one lowercase letter'
+        )
         .matches(/(?=.*?[0-9])/, 'Password must contain at least one number')
-        .matches(/(?=.*?[#?!@$%^&*-])/, 'Password must contain at least one special character')
+        .matches(
+          /(?=.*?[#?!@$%^&*-])/,
+          'Password must contain at least one special character'
+        )
         .min(8, 'Password length must be more than 8 characters'),
       twitter: Yup.string().required('Your twitter handle is required'),
     }),
